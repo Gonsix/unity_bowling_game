3bf4f3ac13ea3ddd660c0d3659484d6ce0c5b3db
Gonsix <gonsix3@gmail.com>
2026-05-23 16:34:31 +0900

    ボールの横方向の位置を決めてそこから投げるバージョン

--- Assets/ShootBall.cs
+++ Assets/ShootBall.cs
@@ -6,6 +6,12 @@ public class ShootBall : MonoBehaviour
 
     private Rigidbody rb;
 
+    // [SerializeField] private Transform ball;
+
+    [SerializeField] private float laneWidth = 4f;
+    [SerializeField] private float shootForce = 3600;
+    private bool nageta = false;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -13,10 +19,27 @@ public class ShootBall : MonoBehaviour
     // 毎フレーム呼び出される
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+
+        if (Input.GetMouseButtonDown(0) && !nageta)
         {
-            Shoot(1200);
+
+            Vector3 mousePos = Input.mousePosition;
+
+            float normalizedZ = mousePos.x / Screen.width;
+
+            float laneZ = Mathf.Lerp(laneWidth / 2f, -laneWidth / 2f, normalizedZ);
+
+            Vector3 newPosition = transform.position;
+
+            newPosition.z = laneZ;
+
+            transform.position = newPosition;
+
+            Shoot(shootForce);
+            nageta = true;
+
         }
+
     }
 
     public void Shoot(float acc)
